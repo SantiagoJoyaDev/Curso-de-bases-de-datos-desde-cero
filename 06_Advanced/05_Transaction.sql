0dc95c7c5bd4910bc3a01c4d3ee9de96ad011312
Santiago Joya <santiagojoyab@outlook.com>
2025-10-31 17:49:19 +0100

    Finalizacion del Fichero 06_Advanced

--- 06_Advanced/05_Transaction.sql
+++ 06_Advanced/05_Transaction.sql
@@ -7,3 +7,9 @@ Comandos básicos:
 ● START TRANSACTION; (o BEGIN;) inicia una nueva transacción.
 ● COMMIT; confirma la transacción, haciendo permanentes todos los cambios realizados desde el inicio de la transacción.
 ● ROLLBACK; revierte todos los cambios hechos en la transacción, dejando*/
+
+
+------------ CONCURRENCY ------------
+
+/*En bases de datos, la concurrencia se refiere a la capacidad del sistema para permitir que múltiples usuarios o procesos accedan y 
+manipulen la base de datos al mismo tiempo, sin que se produzcan errores ni inconsistencias en los datos.*/
